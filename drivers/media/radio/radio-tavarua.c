2b0a63df98be93fd72219e57bc7f15e349495ffe
Venkateshwarlu Domakonda <Venkateshwarlu@codeaurora.org>
2013-03-26 15:32:46 +0530

    radio-tavarua: Update the FM status before configuring the registers
    - Registers are updating with wrong values while Bluetooth
      and FM simultaneously ON.
    - Updating the FM status before configuring the registers.
    
    Change-Id: If5616860a6db9223869193316324ad48e1ff7d31
    CRs-Fixed: 458872
    Signed-off-by: Venkateshwarlu Domakonda <Venkateshwarlu@codeaurora.org>

--- drivers/media/radio/radio-tavarua.c
+++ drivers/media/radio/radio-tavarua.c
@@ -1931,6 +1931,7 @@ static int tavarua_fops_open(struct file *file)
 	if (bahama_present == -ENODEV)
 		return -ENODEV;
 
+	marimba_set_fm_status(radio->marimba, true);
 	if (bahama_present)
 		radio->marimba->mod_id = SLAVE_ID_BAHAMA;
 	else
@@ -2065,7 +2066,6 @@ static int tavarua_fops_open(struct file *file)
 
 	radio->handle_irq = 0;
 	radio->marimba->mod_id = SLAVE_ID_BAHAMA;
-	marimba_set_fm_status(radio->marimba, true);
 	return 0;
 
 
@@ -2081,6 +2081,7 @@ open_err_req_irq:
 config_i2s_err:
 	radio->pdata->fm_shutdown(radio->pdata);
 open_err_setup:
+	marimba_set_fm_status(radio->marimba, false);
 	radio->handle_irq = 1;
 	atomic_inc(&radio->users);
 	return retval;
