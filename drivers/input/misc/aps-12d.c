58d166c734b9f2a8912f743d9e5a2389fe1feee8
Rudolf Tammekivi <rtammekivi@gmail.com>
2014-01-18 15:03:00 +0200

    aps-12d: Update license.

--- drivers/input/misc/aps-12d.c
+++ drivers/input/misc/aps-12d.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013, Rudolf Tammekivi.
+ * Copyright (c) 2013-2014, Rudolf Tammekivi.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
